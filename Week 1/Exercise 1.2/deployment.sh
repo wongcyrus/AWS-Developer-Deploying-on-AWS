d21c2a72197ab3faab388569b6c662eb0e6367f2
EC2 Default User <ec2-user@ip-172-31-70-233.ec2.internal>
2019-02-26 06:49:07 +0000

    Completed Exercise 1.2

--- Week 1/Exercise 1.2/deployment.sh
+++ Week 1/Exercise 1.2/deployment.sh
@@ -4,9 +4,13 @@ SourceBucket=sourcebucketname$AWSAccountId
 aws s3api create-bucket --bucket $SourceBucket
 sleep 5
 aws s3 sync . s3://$SourceBucket --exclude "*" --include "*.yaml"
-aws cloudformation create-stack --stack-name edx-project-stack --template-body file://deplotment.yaml \
+aws s3 cp ~/environment/eb.zip s3://$SourceBucket
+sudo yum install jq -y
+PlatformArn=$(aws elasticbeanstalk list-platform-versions | jq '.PlatformSummaryList |.[] | .PlatformArn' | grep "Python 3.6")
+aws cloudformation create-stack --stack-name edx-project-eb-stack --template-body file://beanstalk.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
---parameters ParameterKey=Password,ParameterValue=P@ssw0rd ParameterKey=SourceBucket,ParameterValue=$SourceBucket 
+--parameters ParameterKey=Password,ParameterValue=P@ssw0rd ParameterKey=SourceBucket,ParameterValue=$SourceBucket \
+ParameterKey=PlatformArn,ParameterValue="$PlatformArn"
 aws cloudformation wait stack-create-complete --stack-name edx-project-stack
 AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name edx-project-stack \
 --query 'Stacks[0].Outputs[?OutputKey==`AccessKey`].OutputValue' --output text)
--- Week 1/Exercise 1.2/lab.sh
+++ Week 1/Exercise 1.2/lab.sh
@@ -1,5 +1,8 @@
-wget https://us-west-2-tcdev.s3.amazonaws.com/courses/AWS-100-ADD/v1.0.0/exercises/ex-beanstalk.zip -O ~/ex-beanstalk.zip
 cd ~/environment
+wget https://us-west-2-tcdev.s3.amazonaws.com/courses/AWS-100-ADD/v1.0.0/exercises/ex-cloud9.zip -O ~/ex-cloud9.zip
+unzip -o ~/ex-cloud9.zip
+sudo pip-3.6 install -r FlaskApp/requirements.txt
+wget https://us-west-2-tcdev.s3.amazonaws.com/courses/AWS-100-ADD/v1.0.0/exercises/ex-beanstalk.zip -O ~/ex-beanstalk.zip
 unzip -o ~/ex-beanstalk.zip
 sed -i 's/REPLACE_WITH_ROOT_PASSWORD/P@ssw0rd/g' FlaskApp/.ebextensions/populate.config
 sed -i 's/REPLACE_WITH_WEB_USER_PASSWORD/P@ssw0rd/g' FlaskApp/.ebextensions/populate.config
