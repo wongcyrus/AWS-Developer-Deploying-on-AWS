905605433de47a0d780cf4ab7bc988dad87b080b
EC2 Default User <ec2-user@ip-172-31-21-248.us-west-2.compute.internal>
2019-02-27 03:39:09 +0000

    Completed Exercise 2.3

--- Week 2/Exercise 2.3/deployment.sh
+++ Week 2/Exercise 2.3/deployment.sh
@@ -4,11 +4,10 @@ SourceBucket=sourcebucketname$AWSAccountId
 aws s3api create-bucket --bucket $SourceBucket
 sleep 5
 aws s3 sync . s3://$SourceBucket --exclude "*" --include "*.yaml"
-aws cloudformation create-stack --stack-name edx-project-codebuild-stack --template-body file://codebuild.yaml \
+aws cloudformation create-stack --stack-name edx-project-codepipeline-stack --template-body file://codepipeline.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameters ParameterKey=SourceBucket,ParameterValue=$SourceBucket \
 ParameterKey=CodeCommitRepoName,ParameterValue=edX-Deploying \
 ParameterKey=CodeBuildProjectName,ParameterValue=UnitTests
-
-aws cloudformation wait stack-create-complete --stack-name edx-project-codebuild-stack
-echo "CodeBuild Stack created!"
+aws cloudformation wait stack-create-complete --stack-name edx-project-codepipeline-stack
+echo "CodePipeline Stack created!"
