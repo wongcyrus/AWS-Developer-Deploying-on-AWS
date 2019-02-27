7a5f35becea781e09a756023dd3160b2fa34c50f
student <123456789@stu.vtc.edu.hk>
2019-02-27 09:51:53 +0000

    Uncomment deployment script

--- Week 3/Exercise 3.2/deployment.sh
+++ Week 3/Exercise 3.2/deployment.sh
@@ -1,16 +1,16 @@
 export AWS_DEFAULT_REGION=us-east-1
 AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
 SourceBucket=sourcebucketname$AWSAccountId
-# aws s3api create-bucket --bucket $SourceBucket
-# sleep 5
-# aws s3 sync . s3://$SourceBucket --exclude "*" --include "*.yaml"
-# aws cloudformation create-stack --stack-name edx-project-codebuild-prepare-stack --template-body file://codebuild.yaml \
-# --capabilities CAPABILITY_NAMED_IAM \
-# --parameters ParameterKey=SourceBucket,ParameterValue=$SourceBucket \
-# ParameterKey=CodeCommitRepoName,ParameterValue=edX-Deploying \
-# ParameterKey=CodeBuildProjectName,ParameterValue=Prepare
-# aws cloudformation wait stack-create-complete --stack-name edx-project-codebuild-prepare-stack
-# echo "CodeBuild Prepare Stack created!"
+aws s3api create-bucket --bucket $SourceBucket
+sleep 5
+aws s3 sync . s3://$SourceBucket --exclude "*" --include "*.yaml"
+aws cloudformation create-stack --stack-name edx-project-codebuild-prepare-stack --template-body file://codebuild.yaml \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameters ParameterKey=SourceBucket,ParameterValue=$SourceBucket \
+ParameterKey=CodeCommitRepoName,ParameterValue=edX-Deploying \
+ParameterKey=CodeBuildProjectName,ParameterValue=Prepare
+aws cloudformation wait stack-create-complete --stack-name edx-project-codebuild-prepare-stack
+echo "CodeBuild Prepare Stack created!"
 
 aws cloudformation update-stack --stack-name edx-project-codepipeline-stack --template-body file://codepipeline.yaml \
 --capabilities CAPABILITY_NAMED_IAM \
