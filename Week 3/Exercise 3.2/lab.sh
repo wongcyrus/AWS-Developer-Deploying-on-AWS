7467c2517bbf705cff92a8e04601285ed734614e
student <123456789@stu.vtc.edu.hk>
2019-02-28 08:22:15 +0000

    Completed Exercise 3.2.

--- Week 3/Exercise 3.2/lab.sh
+++ Week 3/Exercise 3.2/lab.sh
@@ -1,5 +1,7 @@
 yes | cp -rf "../Exercise 3.1/edx-provision.yaml" "../../../Templates/edx-provision.yaml"
+RANDOMCODE=$(date | md5sum)
+sed -i "s/RANDOMCODE/$RANDOMCODE/g" "../../../Templates/edx-provision.yaml"
 cd ~/environment
 git add *
-git commit -m "new owner tag"
+git commit -m "new owner tag with $RANDOMCODE"
 git push origin dev
