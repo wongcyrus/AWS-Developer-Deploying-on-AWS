5cd9f3a941482c880f66bd4c8d46d48f59445043
student <123456789@stu.vtc.edu.hk>
2019-03-05 06:49:21 +0000

    Draft of Exercise 5.3

--- Week 5/Exercise 5.3/deployment.sh
+++ Week 5/Exercise 5.3/deployment.sh
@@ -5,6 +5,11 @@ SourceBucket=sourcebucketname$AWSAccountId
 aws s3api create-bucket --bucket $SourceBucket
 sleep 5
 aws s3 sync . s3://$SourceBucket --exclude "*" --include "*.yaml"
+
+zip code.zip index.js 
+aws s3 cp code.zip s3://$SourceBucket 
+aws logs delete-log-group --log-group-name FlaskApp-Frontends-access-log
+rm code.zip
 aws cloudformation create-stack --stack-name edx-project-es-stack --template-body file://elasticsearch.yaml \
 --capabilities CAPABILITY_NAMED_IAM 
 
--- Week 6/prerequisite.sh
+++ Week 6/prerequisite.sh
@@ -5,5 +5,6 @@ cd "Exercise 5.1"
 sh "deployment.sh"
 cd "../Exercise 5.2"
 sh "lab.sh"
+echo "Wait for Stack Update!"
 sleep 300
 sh "install_cloudwatch.sh"
