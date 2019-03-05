2bf3b51f6c43d3fd3682095367a6e2b030cd391e
EC2 Default User <ec2-user@ip-172-31-14-253.ec2.internal>
2019-03-05 08:35:39 +0000

    Fix prerequiste bug.

--- Week 1/prerequisite.sh
+++ Week 1/prerequisite.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+export AWS_DEFAULT_REGION=us-east-1
+AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
+SourceBucket=sourcebucketname$AWSAccountId
 aws s3api create-bucket --bucket $SourceBucket
 sleep 5
--- Week 6/prerequisite.sh
+++ Week 6/prerequisite.sh
@@ -5,6 +5,4 @@ cd "Exercise 5.1"
 sh "deployment.sh"
 cd "../Exercise 5.2"
 sh "lab.sh"
-echo "Wait for Stack Update!"
-sleep 300
 sh "install_cloudwatch.sh"
