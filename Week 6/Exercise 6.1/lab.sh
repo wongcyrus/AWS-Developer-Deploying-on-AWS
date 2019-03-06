aa8d88e57074bc8afc4fbc79c8eb200ff4ab8cf9
student <123456789@stu.vtc.edu.hk>
2019-03-06 06:14:08 +0000

    Completed Exercise 6.1.

--- Week 6/Exercise 6.1/lab.sh
+++ Week 6/Exercise 6.1/lab.sh
@@ -7,3 +7,14 @@ sed -i 's/REPLACE_WITH_WEB_USER_PASSWORD/P@ssw0rd/g' SetupScripts/create_schema.
 mysql -h localhost -u root -pP@ssw0rd < SetupScripts/create_schema.sql
 cd SetupScripts/
 PASSWORD=P@ssw0rd python3 database_populate.py
+cd ~/environment/FlaskApp
+sh "local_build.sh"
+cd ~/environment/
+git checkout dev
+git add *
+git commit -m "adding japanese language update"
+git push origin dev
+# git log
+git checkout final
+git merge dev
+git push origin final
---
+++ Week 6/Exercise 6.1/local_run.sh
@@ -0,0 +1,6 @@
+cd ~/environment
+export DATABASE_HOST=localhost
+export DATABASE_USER=web_user
+export DATABASE_PASSWORD=P@ssw0rd
+export DATABASE_DB_NAME=routes
+python3 FlaskApp/application.py
