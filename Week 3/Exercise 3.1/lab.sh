84b9bdbc73d17fc8528b138355fce08d8f77312a
student <123456789@stu.vtc.edu.hk>
2019-03-07 08:27:26 +0000

    Fix Exercise 3.1 template bug.

--- Week 3/Exercise 3.1/lab.sh
+++ Week 3/Exercise 3.1/lab.sh
@@ -1,6 +1,7 @@
 cd ~/environment
 wget https://us-west-2-tcdev.s3.amazonaws.com/courses/AWS-100-ADD/v1.0.0/exercises/ex-cfn.zip -O ~/ex-cfn.zip
 unzip -o ~/ex-cfn.zip
+yes | cp -rf "AWS-Developer-Deploying-on-AWS/Week 2/Exercise 3.1/edx-provision.yaml" Templates/edx-provision.yaml
 git add *
 git commit -m "adding CFN to the pipeline"
 git push origin dev
