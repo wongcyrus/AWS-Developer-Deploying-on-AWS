ae263f4fae14e949f4074350e5906c3ad622fab6
student <123456789@stu.vtc.edu.hk>
2019-03-08 01:09:29 +0000

    Fix clean_all typepo.

--- clear_all.sh
+++ clear_all.sh
@@ -2,8 +2,8 @@ export AWS_DEFAULT_REGION=us-east-1
 AWSAccountId=$(aws sts get-caller-identity --query 'Account' --output text)
 aws cloudformation delete-stack --role-arn arn:aws:iam::$AWSAccountId:role/edx-project-codepipeline-stack-CloudFormation --stack-name TEST-Stack
 aws cloudformation delete-stack --role-arn arn:aws:iam::$AWSAccountId:role/edx-project-finalcodepipeline-stack-CloudFormation --stack-name FINAL-Stack
-aws cloudformation wait stack-delete-complete --stack-name edx-project-eb-stack
-aws cloudformation wait stack-delete-complete --stack-name edx-project-eb-stack
+aws cloudformation wait stack-delete-complete --stack-name TEST-Stack
+aws cloudformation wait stack-delete-complete --stack-name FINAL-Stack
 
 for stack in $(aws cloudformation list-stacks --output text --query 'StackSummaries[?contains(StackName, `edx-project-`) && (StackStatus==`CREATE_COMPLETE`||StackStatus==`UPDATE_COMPLETE`) && (!ParentId)].[StackName]') ; \
 do aws cloudformation delete-stack --stack-name $stack --output text; done
